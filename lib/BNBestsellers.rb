4c5420043539710b8058ea5cc69a00db514200f7
Megan <32805437+megdot@users.noreply.github.com>
2018-06-12 23:52:00 -0400

    Create BNBestsellers.rb

---
+++ lib/BNBestsellers.rb
@@ -0,0 +1,9 @@
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
+
+require_relative "./BNBestsellers/version"
+require_relative "./BNBestsellers/cli"
+require_relative "./BNBestsellers/book"
+require_relative "./BNBestsellers/category"
+require_relative "./BNBestsellers/scraper"
