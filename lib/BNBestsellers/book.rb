4797d0bdc4f5cd9c6c0d26a10c2a5ea5c5c61187
Megan <32805437+megdot@users.noreply.github.com>
2018-06-14 22:16:05 -0400

    Update book.rb

--- lib/BNBestsellers/book.rb
+++ lib/BNBestsellers/book.rb
@@ -2,17 +2,24 @@ class BNBestsellers::Book
 
   attr_accessor :name, :author, :rating, :release_date, :book_format, :price, :list_price, :discount, :url, :overview
   
-  def initialize(url = nil)
-    @url = url
-    @all << self
+  @@all = []
+  
+  def initialize(category, attributes)
+    @category = category
+    
+    attributes.each do |key, value|
+      self.send("#{key}=", "#{value}")
+    end
+    
+    @@all << self
   end
   
   def self.all
     @@all
   end
   
-  def self.find(i)
-    self.all[i-1]
+  def open_in_browser
+    system("open '#{url}'")
   end
   
 end
