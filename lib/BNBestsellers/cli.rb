44756945f346de5ffe2e238976a47630aed046d3
Megan <32805437+megdot@users.noreply.github.com>
2018-06-14 21:48:15 -0400

    Update cli.rb

--- lib/BNBestsellers/cli.rb
+++ lib/BNBestsellers/cli.rb
@@ -3,39 +3,62 @@ class BNBestsellers::CLI
   def call
     puts "---------------------------------------------".colorize(:gold)
     puts ""
-    puts "Welcome to the B&N Top 100: Book Bestsellers".colorize(:green)
+    puts "Welcome to Barnes & Nobles Bestsellers!".colorize(:green)
     puts ""
     puts "---------------------------------------------".colorize(:gold)
     puts ""
-    puts "Find your next read by choosing a category! We'll take care of the rest and give you an organized list of top performers!".colorize(:green)
-    puts "Less time searching so you can spend more time reading!".colorize(:green)
-    main_menu
-    exit
+    puts "Please wait momentarily while we retreive this week's bestselling books.".colorize(:green)
+    fiction = Category.new("Fiction Bestsellers", Scraper.scrape_fiction)
+    nonfiction = Category.new("Nonfiction Bestsellers", Scraper.scrape_nonfiction)
+    paperback = Category.new("Paperback Bestsellers", Scraper.scrape_paperback)
+    mass = Category.new("Mass Market Paperback Bestsellers", Scraper.scrape_mass)
+    teens = Category.new("Teens Bestsellers", Scraper.scrape_teens)
+    young_readers = Category.new("Young Readers Bestsellers", Scraper.scrape_young_readers)
+    puts ""
+    puts "Which category would you like to view the top selling books for?"
+    puts ""
+    category_list
+    menu
+  end
+   
+  def category_list
+    Category.print_all
   end
     
-  def main_menu
-    puts "To view a complete list of categories, please enter 'list'.  Otherwise, to exit, please enter 'exit'."
+  def menu
+    puts "To view a complete list of bestselling books by category, please enter the number of the category that you would like to see."  
+    puts ""
+    puts "Otherwise, to exit, please enter 'exit'.".colorize(red)
+    
     input = gets.strip
-    if input == "list"
-      list
+    
+    if @input.to_i.between?(1, Category.all.count)
+      category_bestsellers
     elsif input == "exit"
       exit
     else
-      puts "Invalid entry. We're sorry, we didn't get that.  To view a list of categories, please enter 'list'.  Otherwise, to exit, please enter 'exit'."
+      puts "Invalid entry. Please try again."
     end
   end
 
-  def exit
-    puts "Thank you for visiting.  We hope that you found exactly what you were looking for!"
-    puts "Come back soon!"
-    main_menu
-  end
-
-  def list_books
-    BNBestsellers::Book.all.each.with_index(1) do |book, i|
-      puts "#{i}. #{book.name}"
+  def book_list
+    @the_category = Category.find(@input)
+    
+    puts "Top Books in #{@the_category.name}:"
+    
+    @the_category.books.each.with_index(1) do |book, index|
+      puts " #{index}. #{book.title} by # {book.author}"
     end
+    book
   end
+  
+  def book 
+    puts ""
+    puts "Please type the number of the book you would like more information on:"
+    puts ""
+    
+    book_input = gets.strip
+    book = @the_category.find_book(book_input)
 
   def print_info(book)
     puts "-------------- Name ----------------"
@@ -78,6 +101,30 @@ class BNBestsellers::CLI
     puts ""
     puts "#{book.overview}"
     puts ""
+    
+    puts "Would you like to view this book on your browser? Enter Y/N"
+    browser_input = gets.strip.upcase
+    
+    if browser_input == "Y"
+      book.open_in_browser
+    end
+    
+    puts "Would you like to see another book?  Enter Y/N, or type 'list' to go back to the category list."
+    if input == "Y"
+      book
+    elseif input == "LIST"
+      category_list
+      menu
+    else
+      exit
+    end
   end
+  
+  def exit
+    puts "Thank you for visiting.  We hope that you found exactly what you were looking for!"
+    puts "Come back soon!"
+    main_menu
+  end
+
 
 end
